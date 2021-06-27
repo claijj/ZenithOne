a618f99485501e9470ea3e32e042f5f8a6ee9f48
claijj <33027657+claijj@users.noreply.github.com>
2021-06-27 14:08:29 +0800

    Delete src directory

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { AirTaxiContract } from './air-taxi-contract';
-export { AirTaxiContract } from './air-taxi-contract';
-
-export const contracts: any[] = [ AirTaxiContract ];

59b19eff86602febb948df29c015b14c1ab97172
claijj <33027657+claijj@users.noreply.github.com>
2021-06-28 09:10:07 +0800

    Add files via upload

--- airtaxiservice-app/src/airtaxi-authority.ts
+++ airtaxiservice-app/src/airtaxi-authority.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as readLine from 'readLine';
 
-async function RequestAirTaxi() {
-	console.log('RequestAirTaxi');
+async function ReadAirTaxiHistory(airTaxiID: string) {
+	console.log('ReadAirTaxiHistory');
 
 	try {
 		// Create a new file system based wallet for managing identities.
@@ -26,9 +26,8 @@ async function RequestAirTaxi() {
 		const contract = network.getContract('airtaxiservice-contract');
 		
 		// Submit the specified transaction.
-		let allocatedAirTaxi: Buffer = await contract.submitTransaction('requestAirTaxi');
-		const AirTaxi = (JSON.parse(allocatedAirTaxi.toString()));
-		console.log ('Air Taxi Allocated: ', AirTaxi.TailNumber);
+		let AirTaxiHistory: Buffer = await contract.submitTransaction('readAirTaxiHistory', airTaxiID);
+		console.log (JSON.parse(AirTaxiHistory.toString()));
 				
 		// Disconnect from the gateway.
 		gateway.disconnect();
@@ -74,28 +73,68 @@ async function ReadAirTaxi(airTaxiID: string) {
 	}
 }
 
+async function ApproveFlightPlan() {
+	console.log ('ApproveFlightPlan');
+
+	try {
+		// Create a new file system based wallet for managing identities.
+		const walletPath = path.join(process.cwd(), 'Wallet');
+		const wallet = await Wallets.newFileSystemWallet(walletPath);
+		// console.log(`Wallet path: ${walletPath}`);
+		
+		// Create a new gateway for connecting to our peer node.
+		const gateway = new Gateway();
+		const connectionProfilePath = path.resolve(__dirname, '..',	'connection.json');
+		const connectionProfile =  JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8')); // eslintdisable-line @typescript-eslint/no-unsafe-assignment
+		const connectionOptions = { wallet, identity: 'Org1 Admin', discovery:{ enabled: true, asLocalhost: true } };
+		await gateway.connect(connectionProfile, connectionOptions);
+		
+		// Get the network (channel) our contract is deployed to.
+		const network = await gateway.getNetwork('mychannel');
+
+		// Get the contract from the network.
+		const contract = network.getContract('airtaxiservice-contract');
+		
+		// Submit the specified transaction.
+		await contract.submitTransaction('approveFlightPlan', 'AT0002');
+		// Read-back the approved flight plan
+		ReadAirTaxi('AT0002');
+
+		// Disconnect from the gateway.
+		gateway.disconnect();
+		
+	} catch (error) {
+		console.error('Failed to submit transaction:',error);
+		process.exit(1);
+	}
+}
+
 async function main() {
     const readLine = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    let answer = ""
-    readLine.question("1. Read Air Taxi\n2. Request Air Taxi\n: ", (it: string) => { 
+    let answer = "";
+    readLine.question("1. Read Air Taxi\n2. Read Air Taxi History\n3. Approve Flight Plan\n: ", (it: string) => { 
          answer = it
 		 switch (answer) {
 			 case '1':
-				void ReadAirTaxi('AT0001');	 
+				void ReadAirTaxi('AT0001')		 
 				break;
 
 			case '2':
-				void RequestAirTaxi();
+				void ReadAirTaxiHistory('AT0001')
 				break;
-		 
-			 default:
-				 break;
+			
+			case '3':
+				void ApproveFlightPlan();
+				break;
+
+			default:
+				break;
 		 } 
-         readLine.close();
+         readLine.close()
     })
 }
 
--- airtaxiservice-app/src/airtaxi-operator.ts
+++ airtaxiservice-app/src/airtaxi-operator.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as readLine from 'readLine';
 
-async function RequestAirTaxi() {
-	console.log('RequestAirTaxi');
+async function RegisterAirTaxi() {
+	console.log('RegisterAirTaxi');
 
 	try {
 		// Create a new file system based wallet for managing identities.
@@ -26,9 +26,10 @@ async function RequestAirTaxi() {
 		const contract = network.getContract('airtaxiservice-contract');
 		
 		// Submit the specified transaction.
-		let allocatedAirTaxi: Buffer = await contract.submitTransaction('requestAirTaxi');
-		const AirTaxi = (JSON.parse(allocatedAirTaxi.toString()));
-		console.log ('Air Taxi Allocated: ', AirTaxi.TailNumber);
+		await contract.submitTransaction('registerAirTaxi', 'AT0002', '9V2APP', 'Applane Airways');
+		console.log ('Air taxi AT0002 registered: 9VAPP');
+		// read-back newly registered airtaxi
+		ReadAirTaxi('AT0002');
 				
 		// Disconnect from the gateway.
 		gateway.disconnect();
@@ -74,14 +75,50 @@ async function ReadAirTaxi(airTaxiID: string) {
 	}
 }
 
+async function FileFlightPlan() {
+	console.log ('FileFlightPlan');
+
+	try {
+		// Create a new file system based wallet for managing identities.
+		const walletPath = path.join(process.cwd(), 'Wallet');
+		const wallet = await Wallets.newFileSystemWallet(walletPath);
+		// console.log(`Wallet path: ${walletPath}`);
+		
+		// Create a new gateway for connecting to our peer node.
+		const gateway = new Gateway();
+		const connectionProfilePath = path.resolve(__dirname, '..',	'connection.json');
+		const connectionProfile =  JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8')); // eslintdisable-line @typescript-eslint/no-unsafe-assignment
+		const connectionOptions = { wallet, identity: 'Org1 Admin', discovery:{ enabled: true, asLocalhost: true } };
+		await gateway.connect(connectionProfile, connectionOptions);
+		
+		// Get the network (channel) our contract is deployed to.
+		const network = await gateway.getNetwork('mychannel');
+
+		// Get the contract from the network.
+		const contract = network.getContract('airtaxiservice-contract');
+		
+		// Submit the specified transaction.
+		await contract.submitTransaction('fileFlightPlan','AT0002', 'WSSS', '1 Apr 2024 0800', 'WSSL', '1 Apr 2024 0810', 'Rich Dot Com', 'Ali Anchovies');
+		// Read-back the filed flight plan
+		ReadAirTaxi('AT0002');
+		
+		// Disconnect from the gateway.
+		gateway.disconnect();
+		
+	} catch (error) {
+		console.error('Failed to submit transaction:',error);
+		process.exit(1);
+	}
+}
+
 async function main() {
     const readLine = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    let answer = ""
-    readLine.question("1. Read Air Taxi\n2. Request Air Taxi\n: ", (it: string) => { 
+    let answer = "";
+    readLine.question("1. Read Air Taxi\n2. Register Air Taxi\n3. File Flight Plan\n: ", (it: string) => { 
          answer = it
 		 switch (answer) {
 			 case '1':
@@ -89,11 +126,15 @@ async function main() {
 				break;
 
 			case '2':
-				void RequestAirTaxi();
+				void RegisterAirTaxi();
 				break;
 		 
-			 default:
-				 break;
+			case '3':
+				void FileFlightPlan();
+				break;
+
+			default:
+				break;
 		 } 
          readLine.close();
     })
